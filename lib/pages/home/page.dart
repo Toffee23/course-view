312a33c07da6c19a1235a984bd175c92985909ae
Azag <agboolaodunayo2016@gmail.com>
2023-12-19 21:52:01 +0100

    Update:

--- lib/pages/home/page.dart
+++ lib/pages/home/page.dart
@@ -9,50 +9,86 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15.0),
-            child: Container(
-              height: 130,
-              color: Colors.grey,
-            ),
-          ),
-          const SizedBox(height: 8.0),
-          Padding(
-            padding: const EdgeInsets.only(left: 15.0, right: 2.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                const Text(
-                  'Similar Courses',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                  ),
-                ),
-                TextButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      foregroundColor: MaterialStatePropertyAll(
-                        Theme.of(context).primaryColor,
-                      ),
-                    ),
-                    child: const Text('See more'))
-              ],
-            ),
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            padding: const EdgeInsets.symmetric(horizontal: 15.0),
-            child: Row(
-              children: List.generate(
-                5,
-                (index) => HomeCard(isFirst: index == 0),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
+              child: Container(
+                height: 130,
+                color: Colors.grey,
               ),
             ),
-          )
-        ],
+            const SizedBox(height: 8.0),
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0, right: 2.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  const Text(
+                    'Similar Courses',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  TextButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        foregroundColor: MaterialStatePropertyAll(
+                          Theme.of(context).primaryColor,
+                        ),
+                      ),
+                      child: const Text('See more'))
+                ],
+              ),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
+              child: Row(
+                children: List.generate(
+                  5,
+                  (index) => HomeCard(isFirst: index == 0),
+                ),
+              ),
+            ),
+            const SizedBox(height: 15.0),
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0, right: 2.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  const Text(
+                    'You might like',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  TextButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        foregroundColor: MaterialStatePropertyAll(
+                          Theme.of(context).primaryColor,
+                        ),
+                      ),
+                      child: const Text('See more'))
+                ],
+              ),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
+              child: Row(
+                children: List.generate(
+                  5,
+                  (index) => HomeCard(isFirst: index == 0),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
