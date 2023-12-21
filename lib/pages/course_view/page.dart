4790aadcb151f3af63e7171b64db68ebd59f0f42
Azag <agboolaodunayo2016@gmail.com>
2023-12-21 13:08:41 +0100

    Added video to play

--- lib/pages/course_view/page.dart
+++ lib/pages/course_view/page.dart
@@ -1,8 +1,162 @@
+import 'package:chewie/chewie.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class CourseViewPage extends StatelessWidget {
+class CourseViewPage extends StatefulWidget {
   const CourseViewPage({Key? key}) : super(key: key);
+
+  @override
+  State<CourseViewPage> createState() => _CourseViewPageState();
+}
+
+class _CourseViewPageState extends State<CourseViewPage> {
+  late VideoPlayerController _videoPlayerController1;
+  late VideoPlayerController _videoPlayerController2;
+  ChewieController? _chewieController;
+  int? bufferDelay;
+
+  @override
+  void initState() {
+    initializePlayer();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController1.dispose();
+    _videoPlayerController2.dispose();
+    _chewieController?.dispose();
+    super.dispose();
+  }
+
+  List<String> srcs = [
+    "https://assets.mixkit.co/videos/preview/mixkit-spinning-around-the-earth-29351-large.mp4",
+    "https://assets.mixkit.co/videos/preview/mixkit-daytime-city-traffic-aerial-view-56-large.mp4",
+    "https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1226-large.mp4"
+  ];
+
+  Future<void> initializePlayer() async {
+    _videoPlayerController1 =
+        VideoPlayerController.networkUrl(Uri.parse(srcs[currPlayIndex]));
+    _videoPlayerController2 =
+        VideoPlayerController.networkUrl(Uri.parse(srcs[currPlayIndex]));
+    await Future.wait([
+      _videoPlayerController1.initialize(),
+      _videoPlayerController2.initialize()
+    ]);
+    _createChewieController();
+    setState(() {});
+  }
+
+  void _createChewieController() {
+    // final subtitles = [
+    //     Subtitle(
+    //       index: 0,
+    //       start: Duration.zero,
+    //       end: const Duration(seconds: 10),
+    //       text: 'Hello from subtitles',
+    //     ),
+    //     Subtitle(
+    //       index: 0,
+    //       start: const Duration(seconds: 10),
+    //       end: const Duration(seconds: 20),
+    //       text: 'Whats up? :)',
+    //     ),
+    //   ];
+
+    final subtitles = [
+      Subtitle(
+        index: 0,
+        start: Duration.zero,
+        end: const Duration(seconds: 10),
+        text: const TextSpan(
+          children: <InlineSpan>[
+            TextSpan(
+              text: 'Hello',
+              style: TextStyle(color: Colors.red, fontSize: 22),
+            ),
+            TextSpan(
+              text: ' from ',
+              style: TextStyle(color: Colors.green, fontSize: 20),
+            ),
+            TextSpan(
+              text: 'subtitles',
+              style: TextStyle(color: Colors.blue, fontSize: 18),
+            )
+          ],
+        ),
+      ),
+      Subtitle(
+        index: 0,
+        start: const Duration(seconds: 10),
+        end: const Duration(seconds: 20),
+        text: 'Whats up? :)',
+        // text: const TextSpan(
+        //   text: 'Whats up? :)',
+        //   style: TextStyle(color: Colors.amber, fontSize: 22, fontStyle: FontStyle.italic),
+        // ),
+      ),
+    ];
+
+    _chewieController = ChewieController(
+      videoPlayerController: _videoPlayerController1,
+      // autoPlay: false,
+      // looping: false,
+      // hideControlsTimer: const Duration(seconds: 8),
+      subtitle: Subtitles(subtitles),
+      progressIndicatorDelay:
+          bufferDelay != null ? Duration(milliseconds: bufferDelay!) : null,
+
+      additionalOptions: (context) {
+        return <OptionItem>[
+          OptionItem(
+            onTap: toggleVideo,
+            iconData: Icons.live_tv_sharp,
+            title: 'Toggle Video Src',
+          ),
+        ];
+      },
+      subtitleBuilder: (context, dynamic subtitle) {
+        return Container(
+          color: Colors.yellow,
+          padding: const EdgeInsets.all(10.0),
+          child: subtitle is InlineSpan
+              ? RichText(
+                  text: subtitle,
+                )
+              : Text(
+                  subtitle.toString(),
+                  style: const TextStyle(color: Colors.black),
+                ),
+        );
+      },
+
+      // Try playing around with some of these other options:
+
+      materialProgressColors: ChewieProgressColors(
+        playedColor: Colors.red,
+        handleColor: Colors.blue,
+        backgroundColor: Colors.grey,
+        bufferedColor: Colors.lightGreen,
+      ),
+      placeholder: Container(
+        color: Colors.red,
+      ),
+      // autoInitialize: true,
+    );
+  }
+
+  int currPlayIndex = 0;
+
+  Future<void> toggleVideo() async {
+    await _videoPlayerController1.pause();
+    currPlayIndex += 1;
+    if (currPlayIndex >= srcs.length) {
+      currPlayIndex = 0;
+    }
+    await initializePlayer();
+  }
 
   Widget _subtitleWidget() => Row(
         children: [
@@ -79,6 +233,10 @@ class CourseViewPage extends StatelessWidget {
         ],
       );
 
+  bool get _canPlayVideo =>
+      _chewieController != null &&
+      _chewieController!.videoPlayerController.value.isInitialized;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -102,9 +260,12 @@ class CourseViewPage extends StatelessWidget {
           children: <Widget>[
             AspectRatio(
               aspectRatio: 16 / 9,
-              child: Container(
-                color: Colors.grey.withOpacity(.3),
-              ),
+              child: _canPlayVideo
+                  ? Chewie(controller: _chewieController!)
+                  : Container(
+                      color: Colors.grey.withOpacity(.3),
+                      child: const Center(child: CircularProgressIndicator()),
+                    ),
             ),
             const SizedBox(height: 12.0),
             Text(
